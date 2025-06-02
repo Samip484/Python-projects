fb5135adf6bd181038c8c2c28364d95408bfe754
Samip Thapa <samipthapa484@gmail.com>
2025-06-02 17:56:06 +0545

    Moved Quuiz.py outside new code and deleted new code

--- new code/recursion.py
+++
@@ -1,9 +0,0 @@
-def show_profile(**some):
-    for key,value in some.items():
-        print(f"{key}:{value}")
-
-
-x=input("Enter your name:")
-y=input("Enter your age.")
-z=input("Enter your Roll no:")
-show_profile(name=x,age=y,Roll_no=z)
